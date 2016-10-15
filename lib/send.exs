c755fee77a1b99fcd99a44224a98ecdae46a5e93
Jason Pilz <jasonpilz@gmail.com>
2016-10-14 20:55:47 -0700

    Complete send.exs

--- lib/send.exs
+++ lib/send.exs
@@ -2,3 +2,8 @@
 {:ok, channel}    = AMQP.Channel.open(connection)
 
 AMQP.Queue.declare(channel, "hello")
+
+AMQP.Basic.publish(channel, "", "hello", "Hello, World!")
+IO.puts(" [x] Sent 'Hello, World!'")
+
+AMQP.Connection.close(connection)
